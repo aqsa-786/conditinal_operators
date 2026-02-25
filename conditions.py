fe06e0caecf222b7402b4f275219d0e403ab77e4
Hp <you@example.com>
2026-02-25 15:23:10 +0500

    conditions and operators

---
+++ conditions.py
@@ -0,0 +1,10 @@
+if 100 > 20:
+    print("100 is equal to 20")
+    print("this is me winning the new numbers")
+
+
+marks = int(input("enter your marks:"))
+if marks < 90:
+    print("this is the result of first semester",'marks')
+    percentage = marks/100*100
+    print("your percentage after your hardwork is:",percentage,"%")
---
+++ operators.py
@@ -0,0 +1,12 @@
+#Conditionl Operators
+
+print(2>3)
+print(1<4)
+print(7==7)
+print(2!=3)
+print(50>=45)
+print(2<=5)
+
+list1 = 456
+lis = 12
+print(list1 >= lis)
