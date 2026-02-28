1cb82c365b551caa4782f9cab3a2481b57a9ac39
Hp <you@example.com>
2026-02-28 15:43:20 +0500

    if-else is in the market

---
+++ if-else.py
@@ -0,0 +1,9 @@
+age = int(input("please enter your age"))
+
+if age >= 18:
+
+    print("finally you're reached at the age of maturity and your age is",age,"congratulations")
+else:
+    print("you are not eligible yet because you are only",age,"just be grown up first then come here again later")
+
+    
